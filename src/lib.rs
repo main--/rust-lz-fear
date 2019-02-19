6a849165bee826bcc29e9b2351cf3b7c336c5f5f
johannesvollmer <johannes.boenki@t-online.de>
2019-02-19 14:47:01 +0100

    Lessen the political message in test data by using pandas instead.

--- src/lib.rs
+++ src/lib.rs
@@ -37,14 +37,13 @@ mod tests {
     }
 
     #[test]
-    fn totally_not_edgy_antifa_propaganda() {
-        // extra edginess
-        inverse("The only good fascist is a dead fascist.");
-        inverse("bash the fash");
-        inverse("the fash deserves no bash, only smash");
-        inverse("Dead fascists can't vote.");
-        inverse("Good night, white pride.");
-        inverse("Some say fascism started with gas chambers. I say that's where it ends.");
+    fn save_the_pandas() {
+        inverse("To cute to die! Save the red panda!");
+        inverse("You are 60% water. Save 60% of yourself!");
+        inverse("Save water, it doesn't grow on trees.");
+        inverse("The panda bear has an amazing black-and-white fur.");
+        inverse("The average panda eats as much as 9 to 14 kg of bamboo shoots a day.");
+        inverse("The Empress Dowager Bo was buried with a panda skull in her vault");
     }
 
     #[test]
@@ -76,7 +75,7 @@ mod tests {
 
     #[test]
     fn compression_works() {
-        let s = "micah (Micah Cohen, politics editor): Clinton’s lead has shrunk to a hair above 4 percentage points in our polls-only model, down from about 7 points two weeks ago. So we find ourselves in an odd position where Clinton still holds a clear lead, but it’s shrinking by the day. I’ve been getting questions from Clinton supporters wondering how panicked they should be, and while we advise everyone of all political stripes to always remain calm, let’s try to answer that question today. How safe is Clinton’s lead/how panicked should Democrats be? As tacky as it is to cite your own tweet, I’m going to do it anyway — here’s a handy scale: natesilver: It’s uncertain, in part, because of the risk of a popular vote-Electoral College split. And, in part, because there are various reasons to think polling error could be high this year, such as the number of undecided voters. You can see those forces at play in the recent tightening. Clinton hasn’t really declined very much in these latest polls. But she was at only 46 percent in national polls, and that left a little bit of wiggle room for Trump.";
+        let s = "The Read trait allows for reading bytes from a source. Implementors of the Read trait are called 'readers'. Readers are defined by one required method, read().";
 
         inverse(s);
 
