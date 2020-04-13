5a37858f76e16927e5bf9cf45ff7bab75159820e
main() <main@ehvag.de>
2020-04-13 21:39:48 +0200

    wip

--- examples/better.rs
+++ examples/better.rs
@@ -1,4 +1,4 @@
-use lz4_compression::compress::compress;
+use lz4_compression::compress::{compress,compress2,U32Table};
 use std::fs::File;
 use std::io::{self, Write, Read};
 use std::env;
@@ -6,14 +6,22 @@ use std::env;
 fn main() -> io::Result<()> {
     let filename_in = env::args().skip(1).next().unwrap();
     let filename_out = env::args().skip(2).next().unwrap();
+    let filename_out2 = env::args().skip(3).next().unwrap();
     let mut file_in = File::open(filename_in)?;
     let mut file_out = File::create(filename_out)?;
+    let mut file_out2 = File::create(filename_out2)?;
 
     let mut buf = Vec::new();
     file_in.read_to_end(&mut buf)?;
 
+    let mut compressed2 = Vec::new();
+    compress2::<_, U32Table>(&buf, &mut compressed2)?;
     let compressed = compress(&buf);
+
+//    assert!(compressed.iter().eq(compressed2.iter()));
+
     file_out.write_all(&compressed)?;
+    file_out2.write_all(&compressed2)?;
 //    assert!(lz4_compression::decompress::decompress(&compressed).unwrap().iter().copied().eq(buf));
 
 /*
--- src/compress.rs
+++ src/compress.rs
@@ -4,8 +4,13 @@
 //! high performance. It has fixed memory usage, which contrary to other approachs, makes it less
 //! memory hungry.
 
+use std::cmp;
 use std::io::Read;
-use byteorder::{ByteOrder, NativeEndian, ReadBytesExt, LE};
+use std::convert::TryInto;
+use byteorder::{ByteOrder, NativeEndian, ReadBytesExt, WriteBytesExt, LE};
+use fehler::{throws};
+
+type Error = std::io::Error;
 
 /// Duplication dictionary size.
 ///
@@ -13,7 +18,52 @@ use byteorder::{ByteOrder, NativeEndian, ReadBytesExt, LE};
 /// thus collisions are more likely, hurting the compression ratio.
 const DICTIONARY_SIZE: usize = 1 << HASHLOG;
 const HASHLOG: usize = 12;
+const MINMATCH: usize = 4;
 
+
+pub trait EncoderTable: Default {
+    fn get(&self, key: &[u8]) -> usize;
+    // value is declared as usize but must not be above payload_size_limit
+    fn set(&mut self, key: &[u8], value: usize);
+    fn payload_size_limit() -> usize;
+}
+
+pub struct U32Table {
+    dict: [u32; DICTIONARY_SIZE],
+}
+impl Default for U32Table {
+    fn default() -> Self {
+        U32Table { dict: [0; DICTIONARY_SIZE] }
+    }
+}
+
+
+fn hash5(input: &[u8]) -> usize {
+    // read 64 bits as 4+1 bytes
+    let upper_byte = input.get(4).copied().unwrap_or(0);
+    let v = NativeEndian::read_u32(input) as u64 | ((upper_byte as u64) << 32);
+
+    // calculate a checksum
+    ((v << 24).wrapping_mul(889523592379) as usize) >> (64 - HASHLOG)
+}
+
+impl EncoderTable for U32Table {
+    fn get(&self, key: &[u8]) -> usize {
+        self.dict[hash5(key)].try_into().expect("This code is not supposed to run on a 16 arch (let alone smaller)")
+    }
+    fn set(&mut self, key: &[u8], value: usize) {
+        self.dict[hash5(key)] = value.try_into().expect("EncoderTable contract violated");
+    }
+    fn payload_size_limit() -> usize { u32::MAX as usize }
+}
+/*
+struct U16Table {
+    dict: [u16; DICTIONARY_SIZE*2],
+}
+impl EncoderTable for U16Table {
+     fn payload_size_limit() -> usize { u16::MAX as usize }
+}
+*/
 
 /// A LZ4 block.
 ///
@@ -43,7 +93,7 @@ struct Duplicate {
 }
 
 /// An LZ4 encoder.
-pub struct Encoder<'a> {
+pub struct Encoder<'a, T> {
     /// The raw uncompressed input.
     input: &'a [u8],
 
@@ -59,10 +109,14 @@ pub struct Encoder<'a> {
     ///
     /// Every four bytes are hashed, and in the resulting slot their position in the input buffer
     /// is placed. This way we can easily look up a candidate to back references.
-    dict: [u32; DICTIONARY_SIZE],
+    table: T,
 }
 
-impl<'a> Encoder<'a> {
+fn eq4bytes(a: &[u8], b: &[u8]) -> bool {
+    NativeEndian::read_u32(a) == NativeEndian::read_u32(b)
+}
+
+impl<'a, T: EncoderTable> Encoder<'a, T> {
     /// Go forward by some number of bytes.
     ///
     /// This will update the cursor and dictionary to reflect the now processed bytes.
@@ -111,7 +165,8 @@ impl<'a> Encoder<'a> {
             
 //            println!("inserting@{:04} {:016x}", self.cur, self.get_batch_at_cursor().swap_bytes());
             
-            self.dict[self.get_cur_hash()] = self.cur as u32;
+            self.table.set(&self.input[self.cur..], self.cur);
+//            self.dict[self.get_cur_hash()] = self.cur as u32;
         }
     }
 
@@ -120,18 +175,10 @@ impl<'a> Encoder<'a> {
         self.cur + 4 < self.input.len()
     }
 
-    /// Get the hash of the current four bytes below the cursor.
-    ///
-    /// This is guaranteed to be below `DICTIONARY_SIZE`.
-    fn get_cur_hash(&self) -> usize {
-        let v = self.get_batch_at_cursor();
-        ((v << 24).wrapping_mul(889523592379) as usize) >> (64 - HASHLOG)
-    }
-
     /// Read a 4-byte "batch" from some position.
     ///
     /// This will read a native-endian 4-byte integer from some position.
-    fn get_batch(&self, n: usize) -> u64 {
+    fn get_batch(&self, n: usize) -> &[u8] {
         assert!(self.remaining_batch(), "Reading a partial batch.");
 
 	//let xo = if (n + 8) <= self.input.len() { NativeEndian::read_u32(&self.input[n+4..]) as u64 } else { 0 };
@@ -140,13 +187,9 @@ impl<'a> Encoder<'a> {
 	//(&self.input[n..]).chain(zeroes).read_u64::<LE>().unwrap()
 	//(&self.input[n..]).chain(zeroes).read_u32::<LE>().unwrap() as u64
 	
-	let upper_byte = self.input.get(n+4).copied().unwrap_or(0);
-        NativeEndian::read_u32(&self.input[n..]) as u64 | ((upper_byte as u64) << 32)
-    }
-
-    /// Read the batch at the cursor.
-    fn get_batch_at_cursor(&self) -> u64 {
-        self.get_batch(self.cur)
+//	let upper_byte = self.input.get(n+4).copied().unwrap_or(0);
+//        NativeEndian::read_u32(&self.input[n..]) as u64 | ((upper_byte as u64) << 32)
+        &self.input[n..]
     }
 
     /// Find a duplicate of the current batch.
@@ -158,8 +201,10 @@ impl<'a> Encoder<'a> {
             return None;
         }
 
+        let current_batch = self.get_batch(self.cur);
         // Find a candidate in the dictionary by hashing the current four bytes.
-        let candidate = self.dict[self.get_cur_hash()] as usize;
+//        let candidate = self.dict[self.get_cur_hash()] as usize;
+        let candidate = self.table.get(current_batch);
 
         // Three requirements to the candidate exists:
         // - The candidate is not the trap value (0xFFFFFFFF), which represents an empty bucket.
@@ -168,7 +213,8 @@ impl<'a> Encoder<'a> {
         // - We can address up to 16-bit offset, hence we are only able to address the candidate if
         //   its offset is less than or equals to 0xFFFF.
         if self.cur != 0 //candidate != !0
-            && (self.get_batch(candidate) as u32) == (self.get_batch_at_cursor() as u32)
+            && eq4bytes(self.get_batch(candidate), current_batch)
+//            && (self.get_batch(candidate) as u32) == (self.get_batch_at_cursor() as u32)
             && self.cur - candidate <= 0xFFFF {
 
             // Calculate the "extension bytes", i.e. the duplicate bytes beyond the batch. These
@@ -187,12 +233,13 @@ impl<'a> Encoder<'a> {
 //            for (a, b) in self.input[self.cur+4..].chunks_exact(4).zip(self.input[candidate+4..].chunks_exact(4)) {
             for (a, b) in self.input[self.cur+4..].chunks(4).zip(self.input[candidate+4..].chunks(4)) {
                 // as the candidate is always behind our cursor, a is always smaller
-                if a.len() != 4 || /*b.len() != 4*/ {
+                if a.len() != 4 /*|| b.len() != 4*/ {
                     // slow path, can't read full integers
                     ext += a.iter().zip(b).take_while(|&(a, b)| a == b).count();
                     break;
                 }
 
+// TODO: can do 64 bits here
 //                println!("{:?} vs {:?}", a, b);
                 let a = NativeEndian::read_u32(a);
                 let b = NativeEndian::read_u32(b);
@@ -356,9 +403,140 @@ pub fn compress_into(input: &[u8], output: &mut Vec<u8>) {
         output,
         cur: 0,
 //        dict: [!0; DICTIONARY_SIZE],
-        dict: [0; DICTIONARY_SIZE],
+        table: U32Table::default(), //[0; DICTIONARY_SIZE],
     }.complete();
 }
+
+
+#[cfg(target_endian = "little")] fn archdep_zeros(i: u32) -> u32 { i.trailing_zeros() }
+#[cfg(target_endian = "big")] fn archdep_zeros(i: u32) -> u32 { i.leading_zeros() }
+
+fn count_matching_bytes(a: &[u8], b: &[u8]) -> usize {
+     let mut matching_bytes = 0;
+     // match in chunks of 4 bytes so we process an 32 bits at a time
+     for (a, b) in a.chunks_exact(4).zip(b.chunks_exact(4)) {
+        let a = NativeEndian::read_u32(a);
+        let b = NativeEndian::read_u32(b);
+        let xor = a ^ b;
+        if xor == 0 {
+            matching_bytes += 4;
+        } else {
+            // optimization: if it doesn't match at all, don't even bother
+            // TODO: benchmark whether this is worth
+            if matching_bytes != 0 {
+                matching_bytes += archdep_zeros(xor) as usize / 8;
+            }
+            return matching_bytes;
+        }
+    }
+    
+    // we only return here if we ran out of data (i.e. all 4-byte blocks have matched)
+    // but there may be up to 3 more bytes to check!
+    let trailing_matches = a.iter().zip(b).skip(matching_bytes).take_while(|&(a, b)| a == b).count();
+    //let trailing_matches = a[matching_bytes..].iter().zip(&b[matching_bytes..]).take_while(|&(a, b)| a == b).count();
+    matching_bytes + trailing_matches
+}
+
+use std::io::{Write, Cursor};
+#[throws]
+pub fn compress2<W: Write, T: EncoderTable>(input: &[u8], mut writer: W) {
+    let mut table = T::default();
+
+    let mut cursor = 0;
+    while cursor < input.len() {
+        let literal_start = cursor;
+        
+        // look for a duplicate
+        let duplicate = loop {
+            if (input.len() - cursor) < 4 {
+                // end with a literal-only section
+                let literal_len = input.len() - literal_start;
+                
+                let mut token = 0;
+                write_lsic_head(&mut token, 4, input.len() - literal_start);
+                writer.write_u8(token);
+                write_lsic_tail(&mut writer, literal_len)?;
+                writer.write_all(&input[literal_start..][..literal_len])?;
+                return;
+            }
+        
+            let current_batch = &input[cursor..];
+            let candidate = table.get(current_batch);
+            table.set(current_batch, cursor);
+
+            if (cursor != 0) // can never match on the very first byte
+                && cursor - candidate <= 0xFFFF { // must be an addressable offset
+                // let's see how many matching bytes we have
+                let candidate_batch = &input[candidate..];
+                let matching_bytes = count_matching_bytes(current_batch, candidate_batch);
+
+                if let Some(mut extra_bytes) = matching_bytes.checked_sub(MINMATCH) {
+                    // if it wasn't, this was just a hash collision :(
+                    let offset = (cursor - candidate) as u16;
+
+        // backtrack
+        let max_backtrack = cmp::min(cursor - literal_start, (u16::MAX - offset) as usize);
+        let backtrack = input[..cursor].iter().rev().zip(input[..candidate].iter().rev()).take(max_backtrack).take_while(|&(a, b)| a == b).count();
+        // offset remains unchanged
+        //cursor -= backtrack;
+        extra_bytes += backtrack;
+
+        cursor += matching_bytes;
+        println!("{} of {}", cursor, input.len());
+        
+        let minus_two = &input[cursor-2..];
+        if minus_two.len() >= 4 {
+        table.set(minus_two, cursor-2);
+        }
+//        table.set(&input[cursor..], cursor);
+        
+                    break Duplicate { offset, extra_bytes };
+                }
+            }
+            
+            // no match, keep looping
+//            println!("inserting@{:04} {:?}", cursor,  &current_batch[..8]);
+
+//            table.set(current_batch, cursor);
+            cursor += 1;
+        };
+        
+        // cursor is now pointing past the match
+        let literal_end = cursor - duplicate.extra_bytes - MINMATCH;
+        let literal_len = literal_end - literal_start;
+        
+            println!("loopy {} {:?}", literal_len, duplicate);
+        
+        let mut token = 0;
+        write_lsic_head(&mut token, 4, literal_len);
+        write_lsic_head(&mut token, 0, duplicate.extra_bytes);
+
+        writer.write_u8(token);
+        write_lsic_tail(&mut writer, literal_len)?;
+        writer.write_all(&input[literal_start..literal_end])?;
+        writer.write_u16::<LE>(duplicate.offset)?;
+        write_lsic_tail(&mut writer, duplicate.extra_bytes)?;
+   }
+}
+fn write_lsic_head(token: &mut u8, shift: usize, value: usize) {
+    let i = cmp::min(value, 0xF) as u8;
+    *token |= i << shift;
+}
+#[throws]
+fn write_lsic_tail<W: Write>(writer: &mut W, mut value: usize) {
+    if value < 0xF {
+        return;
+    }
+
+    value -= 0xF;
+
+    while value >= 0xFF {
+        writer.write_u8(0xFF);
+        value -= 0xFF;
+    }
+    writer.write_u8(value as u8);
+}
+
 
 /// Compress all bytes of `input`.
 pub fn compress(input: &[u8]) -> Vec<u8> {
