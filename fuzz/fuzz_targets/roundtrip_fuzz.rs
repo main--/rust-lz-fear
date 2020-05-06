039045a3a0b3e3e0915ef3b3e236caf7d92793e0
main() <main@ehvag.de>
2020-05-06 10:07:05 +0200

    Update fuzzing code
    * there is no minimum input size
    * verify we read the same data

--- fuzz/fuzz_targets/roundtrip_fuzz.rs
+++ fuzz/fuzz_targets/roundtrip_fuzz.rs
@@ -1,24 +1,23 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use lz_fear::framed::{CompressionSettings, LZ4FrameReader};
-use std::io::Cursor;
+use std::io::{Cursor, Read};
 
 fuzz_target!(|data: &[u8]| {
-    // TODO - What's the minimum input size for lz4?
-    if data.len() >= 31 {
-        let input = Cursor::new(data);
-        let mut output = Vec::new();
+    let input = Cursor::new(data);
+    let mut output = Vec::new();
 
-        CompressionSettings::default()
-            .content_checksum(true)
-            .independent_blocks(true)
-            .compress_with_size(input, &mut output)
-            .expect("Could not compress input data");
+    CompressionSettings::default()
+        .content_checksum(true)
+        .independent_blocks(true)
+        .compress(input, &mut output) //_with_size(input, &mut output)
+        .expect("Could not compress input data");
 
-        let mut lz4_reader = LZ4FrameReader::new(Cursor::new(output))
-            .expect("Could not create frame reader")
-            .into_read();
+    let mut lz4_reader = LZ4FrameReader::new(Cursor::new(output))
+        .expect("Could not create frame reader")
+        .into_read();
 
-        // TODO - Completely decompress and compare decompressed output to initial fuzz input
-    }
+    let mut roundtripped = Vec::new();
+    lz4_reader.read_to_end(&mut roundtripped).expect("Could not read decompressed data");
+    assert!(roundtripped.iter().eq(data));
 });
